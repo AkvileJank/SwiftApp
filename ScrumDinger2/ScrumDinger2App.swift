e638f7bcce1ad67b86d629c9d0829fe509de1abd
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2023-03-05 18:37:05 +0200

    Initial Commit

---
+++ ScrumDinger2/ScrumDinger2App.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDinger2App.swift
+//  ScrumDinger2
+//
+//  Created by Akvile on 2023-03-05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDinger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
